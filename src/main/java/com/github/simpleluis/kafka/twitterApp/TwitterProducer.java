67d1a44b7bd847dedea55f9a9fd982baac74d276
LuisCastilloH <andres.castillo.hdez@gmail.com>
2020-10-17 19:42:36 -0500

    Added kafka settings

--- src/main/java/com/github/simpleluis/kafka/twitterApp/TwitterProducer.java
+++ src/main/java/com/github/simpleluis/kafka/twitterApp/TwitterProducer.java
@@ -30,7 +30,7 @@ public class TwitterProducer {
 
     Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
 
-    List<String> terms = Lists.newArrayList("kafka", "bitcoin");
+    List<String> terms = Lists.newArrayList("kafka", "bitcoin", "football");
     String consumerKey;
     String consumerSecret;
     String token;
@@ -138,6 +138,17 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create safe Producer properties
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // high throughput producer (latency and CPU go up)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
 
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
